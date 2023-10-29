5cfa1124a249570108ca006f7bcdee96878c59e9
bigdefence <wjdrkdqls12@naver.com>
2023-10-30 01:48:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ def main():
     
     # 파일 업로드 섹션 디자인
     st.subheader('웹툰속으로에서는 단순한 클릭 한 번으로 당신의 사진을 독특하고 재미있는 웹툰 스타일로 변환해드립니다!:sunglasses:')
-    st.write(':blue[사진은 저장되지 않습니다!]')
+    st.write(':blue[얼굴 사진을 업로드 해주세요! 사진은 저장되지 않습니다!]')
     # 파일 업로드 컴포넌트
     uploaded_file = st.file_uploader("PNG 또는 JPG 이미지를 업로드하세요.", type=["png", "jpg", "jpeg"])
     if uploaded_file is not None:
